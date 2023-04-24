ce2d2254f1920a1a78c6e4bb451e6105c6f0758b
Travis Kroll <tjkroll@mail.csuchico.edu>
2023-04-23 21:16:46 -0700

    got it

--- appreticeship/interview.js
+++ appreticeship/interview.js
@@ -7,7 +7,7 @@ app.get('/', (req, res)=>{
     res.json({
         "message": "My name is Travis Kroll",
         "timestamp": Date.now(),
-        "new": "howdy",
+        "new": "got it!",
     }); 
 }); 
 
